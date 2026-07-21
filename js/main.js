b97794e551890729c0ae9e6c6a8028bcc0df57d8
Genn <fotoxsppua@gmail.com>
2026-07-21 19:24:25 +0300

    tt

--- js/main.js
+++ js/main.js
@@ -1,9 +1,17 @@
 class ModalService {
-  constructor() {
-    console.log(this); console.log(this);
-  }
+  #modal;
 
+  // 1. Додаємо параметр selector у конструктор
+  constructor(selector) {
+    this.#modal = document.querySelector(selector);
+    
+    console.log('Екземпляр класу (this):', this);
+    console.log('Знайдений елемент:', this.#modal);
+  }
+} // 2. Не забудьте закрити клас тут!
+
+// 3. Правильно оформлюємо стрілочну функцію () => { ... }
 document.addEventListener('DOMContentLoaded', () => {
-  // Тепер можна передавати просто рядок
+  // Передаємо рядок з селектором
   const modal = new ModalService('.hide');
 });
