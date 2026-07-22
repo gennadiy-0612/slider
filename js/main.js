4ea52d3a6afb7431be222f35b3a90ad12451a7cf
Genn <fotoxsppua@gmail.com>
2026-07-22 18:40:57 +0300

    Fix console log formatting in ModalService constructor

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ class ModalService {
   constructor(selector) {
     this.#modal = document.querySelector(selector);
     
-    console.log('Екземпляр класу (this):', this);
+    console.log('Екземпляр класу (this):', this); 
     console.log('Знайдений елемент:', this.#modal);
   }
 } // 2. Не забудьте закрити клас тут!
