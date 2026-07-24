9d5eb56538a6589dda460c757ae3b75e2215443f
Genn <fotoxsppua@gmail.com>
2026-07-24 15:50:33 +0300

    Refactor ElHtml class: streamline constructor, improve element creation logic, and enhance attribute handling

--- js/main.js
+++ js/main.js
@@ -1,34 +1,61 @@
-const img = {}
 class ElHtml {
-  #TagName;
-  #CLASS;
-  #ALT;
-  #SRC;
-  constructor(DataSet) {
-    this.#TagName = DataSet.TagName;
-    this.#CLASS = DataSet.classes;
-    this.#ALT = DataSet.alt;
-    this.#SRC = DataSet.src;
-    console.log(this);
-    for (const [key, value] of Object.keys(DataSet)) {
-      console.log(`${key}: ${value}`);
+  #tag;
+  #parent;
+  #classes;
+  #attrs = {};
+
+  constructor(config) {
+    // 1. Циклом перебираємо об'єкт конфігурації
+    for (const [key, value] of Object.entries(config)) {
+      if (key === "tag") this.#tag = value;
+      else if (key === "parent") this.#parent = value;
+      else if (key === "classes") this.#classes = value;
+      else this.#attrs[key] = value; // Усі інші поля (src, alt, href, id...) зберігаємо як атрибути
     }
   }
-  SHWO() {
-    const el = document.createElement(this.#TagName);
-    el.papa = document.querySelectorAll(".slider");
-    el.papa[0].appendChild(el);
-    el.setAttribute("class", this.#CLASS);
-    el.setAttribute("alt", this.#ALT);
-    el.setAttribute("src", this.#SRC);
+
+  SHOW() {
+    // Створюємо елемент
+    const el = document.createElement(this.#tag || "div");
+
+    // Додаємо класи
+    if (this.#classes) el.className = this.#classes;
+
+    // Застосовуємо всі зібрані атрибути та властивості (src, alt, textContent і т.д.)
+    Object.assign(el, this.#attrs);
+
+    // Додаємо в батік або за замовчуванням у body
+    const parentEl = document.querySelector(this.#parent) || document.body;
+    parentEl.appendChild(el);
+
     return el;
   }
 }
-img.TagName = "img";
-img.classes = "ttt";
-img.alt = "Description of the image";
-img.src = "img/w.png";
-const ElHtmls = new ElHtml(img);
+
+// ==========================================
+// ПРИКЛАДИ БАГАТОРАЗОВОГО ВИКОРИСТАННЯ:
+// ==========================================
+
+// 1. Створення зображення
+const sliderImg = new ElHtml({
+  parent: ".slider",
+  tag: "img",
+  classes: "ttt main-img",
+  src: "img/w.png",
+  alt: "Description of the image"
+});
+
+// 2. Створення посилання
+const link = new ElHtml({
+  parent: ".slider",
+  tag: "a",
+  classes: "btn-link",
+  href: "https://google.com",
+  target: "_blank",
+  textContent: "Перейти на сайт"
+});
+
 document.addEventListener("DOMContentLoaded", () => {
-  ElHtmls.SHWO();
+  sliderImg.SHOW();
+  link.SHOW();
 });
