af5a29ecd1d1c36ba933c49262d9d4c5e8c26d4c
Genn <fotoxsppua@gmail.com>
2026-07-24 17:33:30 +0300

    Refactor ElHtml class: enhance configuration handling, unify class property, and improve event listener flexibility

--- js/main.js
+++ js/main.js
@@ -1,39 +1,44 @@
 class ElHtml {
-  #tag; #parent; #classes; #attrs = {};
+  #tag; #parent; #class; #attrs = {}; #listen = {}; #textContent;
 
-  constructor(config) {
-    for (const [key, value] of Object.entries(config)) {
-      if (key === "tag") this.#tag = value;
-      else if (key === "parent") this.#parent = value;
-      else if (key === "classes") this.#classes = value;
-      else this.#attrs[key] = value;
-    }
+constructor(config) {
+  for (const [key, value] of Object.entries(config)) {
+    if (key === "tag") this.#tag = value;
+    else if (key === "parent") this.#parent = value;
+    else if (key === "class") this.#class = value;
+    else if (key === "textContent") this.#textContent = value;
+    else this.#attrs[key] = value;
   }
+}
 
   SHOW() {
     const el = document.createElement(this.#tag || "div");
-    if (this.#classes) el.className = this.#classes;
+    if (this.#class) el.className = this.#class;
     Object.assign(el, this.#attrs);
-
+    if (this.#listen.event) {
+      el.addEventListener(this.#listen.event, this.#listen.deal);
+    }
     const parentEl = document.querySelector(this.#parent) || document.body;
     parentEl.appendChild(el);
     return el;
   }
-
+ static ADD_LISTENER(el, event, deal) {
+    el.addEventListener(event, deal);
+  }
+static makeIt(ttt) {
+  console.log(ttt);
+}
   // СТАТИЧНИЙ МЕТОД: приймає масив конфігурацій і показує всі
   static SHOW_ALL(configsArray) {
     return configsArray.map(config => new ElHtml(config).SHOW());
-  }
-  static RadioStation(els) {
-    console.log(this);
   }
 }
 
 // ВИКОРИСТАННЯ В 1 РЯДОК:
 document.addEventListener("DOMContentLoaded", () => {
   ElHtml.SHOW_ALL([
-    { parent: "form", tag: "input", placeholder: "Логін" },
-    { parent: "form", tag: "input", type: "password", placeholder: "Пароль" },
-    { parent: "form", tag: "button", textContent: "Увійти" }
+    { parent: ".slider", tag: "img", class: "slider-img", src: "img/w.png", alt: "Slider Image 1", listen: { event: "click", deal: () => console.log("Image clicked!") } },
+    { parent: ".slider", tag: "p", class: "password", textContent: "Password: 12345" },
+    { parent: ".slider", tag: "span", class: "password" }
   ]);
-});
+})
