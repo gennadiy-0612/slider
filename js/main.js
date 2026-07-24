86284a92355222639318d3fd684bd6267e1cb804
Genn <fotoxsppua@gmail.com>
2026-07-24 15:23:59 +0300

    Refactor ElHtml class: update property names for consistency, improve structure, and enhance element creation logic

--- js/main.js
+++ js/main.js
@@ -1,25 +1,34 @@
-class ElHtml{
-  #TagName
-  #ClName;
-  constructor(DataSet){
+const img = {}
+class ElHtml {
+  #TagName;
+  #CLASS;
+  #ALT;
+  #SRC;
+  constructor(DataSet) {
     this.#TagName = DataSet.TagName;
-    this.#ClName = DataSet.ClName;
+    this.#CLASS = DataSet.classes;
+    this.#ALT = DataSet.alt;
+    this.#SRC = DataSet.src;
+    console.log(this);
+    for (const [key, value] of Object.keys(DataSet)) {
+      console.log(`${key}: ${value}`);
+    }
   }
-  SHWO(){
+  SHWO() {
     const el = document.createElement(this.#TagName);
-    el.className = this.#ClName;
-    console.log(el);
+    el.papa = document.querySelectorAll(".slider");
+    el.papa[0].appendChild(el);
+    el.setAttribute("class", this.#CLASS);
+    el.setAttribute("alt", this.#ALT);
+    el.setAttribute("src", this.#SRC);
     return el;
   }
 }
-const imgs = {
-  alt: "Description of the image",
-  src: "img/w.png"
-}
-const ElHtmls = new ElHtml({TagName: "img", ClName: "my-image"});
-const imgElement = ElHtmls.SHWO();
-imgElement.src = imgs.src;
-imgElement.alt = imgs.alt;
+img.TagName = "img";
+img.classes = "ttt";
+img.alt = "Description of the image";
+img.src = "img/w.png";
+const ElHtmls = new ElHtml(img);
 document.addEventListener("DOMContentLoaded", () => {
   ElHtmls.SHWO();
 });
