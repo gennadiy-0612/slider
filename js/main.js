29319c7f5fadacf9e688baaa5bffb26c2d3d221b
Genn <fotoxsppua@gmail.com>
2026-07-24 11:54:33 +0300

    Add image file and update script reference in HTML for improved functionality

--- js/main.js
+++ js/main.js
@@ -1,7 +1,25 @@
 class ElHtml{
   #TagName
   #ClName;
-  constructor(){
-
+  constructor(DataSet){
+    this.#TagName = DataSet.TagName;
+    this.#ClName = DataSet.ClName;
+  }
+  SHWO(){
+    const el = document.createElement(this.#TagName);
+    el.className = this.#ClName;
+    console.log(el);
+    return el;
   }
 }
+const imgs = {
+  alt: "Description of the image",
+  src: "img/w.png"
+}
+const ElHtmls = new ElHtml({TagName: "img", ClName: "my-image"});
+const imgElement = ElHtmls.SHWO();
+imgElement.src = imgs.src;
+imgElement.alt = imgs.alt;
+document.addEventListener("DOMContentLoaded", () => {
+  ElHtmls.SHWO();
+});
